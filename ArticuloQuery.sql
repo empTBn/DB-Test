339556a87f48f704aac1c47231e32fb7ec8f8006
Emmanuel MJ <85372632+empTBn@users.noreply.github.com>
2023-08-24 12:31:25 -0600

    Add files via upload
    Test Query for Articulo

---
+++ ArticuloQuery.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO Articulo (Nombre, Precio)
+VALUES
+	('Martillo', 10000),
+	('Aspiradora', 15000),
+    ('Mesa', 5000),
+    ('Computadora', 110000),
+    ('Pizarra', 12000),
+    ('Zapatos deportivos', 11000);
+
+/*
+DELETE FROM Articulo
+WHERE Id = 7;
+
+UPDATE Articulo
+SET Precio = 10000
+WHERE Id = 10;
+*/
+
+
+SELECT * FROM Articulo;
+
